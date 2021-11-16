3ae6e7e945aeef1965445c4f843fa9307207bb33
Alexander Matzen <alexander@amatzen.dk>
2021-11-16 09:58:37 +0100

    Added router

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueRouter from "vue-router";
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(VueRouter)
+    .mount('#app')

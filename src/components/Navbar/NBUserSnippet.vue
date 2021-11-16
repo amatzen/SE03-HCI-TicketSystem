b6363538238c9881053277ad2ea4ae1112bee472
Alexander Matzen <alexander@amatzen.dk>
2021-11-16 10:43:59 +0100

    Navbar default

---
+++ src/components/Navbar/NBUserSnippet.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>{{ name }}</p>
+    <p>{{ position }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NBUserSnippet",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    position: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,12 +1,26 @@
 <template>
-  <header>
-    <p>Ticket<span class="text-green-500">o</span></p>
+  <header class="py-6 mb-5 bg-white shadow-md">
+    <div class="container mx-auto flex">
+      <span class="font-bold">Ticket<span class="text-green-500">o</span></span>
+      <nav>
+        <ul>
+          <!-- TODO: Set up router links instead of default a tags -->
+          <li><a href="/">Oversigt</a></li>
+          <li><a href="/">Kundeadministration</a></li>
+          <li><a href="/">Statistik</a></li>
+        </ul>
+      </nav>
+
+      <NBUserSnippet name="Dorte" position="Kunderådgiver" />
+    </div>
   </header>
 </template>
 
 <script>
+import NBUserSnippet from "./NBUserSnippet.vue";
 export default {
-  name: "Navbar"
+  name: "Navbar",
+  components: {NBUserSnippet}
 }
 </script>
 

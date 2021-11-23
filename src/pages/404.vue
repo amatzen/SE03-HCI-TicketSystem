759c92d797d78745e4d0b8adcb3b75fc72718e23
Alexander Matzen <alexander@amatzen.dk>
2021-11-23 15:09:07 +0100

    404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Der skete en fejl</h1>
+    <p>Vi kunne ikke finde den efterspurgte side</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFoundPage"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/routes.ts
+++ src/pages/routes.ts
@@ -3,6 +3,7 @@ import {RouteRecordRaw} from "vue-router";
 import DefaultLayout from '../layouts/DefaultLayout.vue';
 
 import OverviewPage from './Overview/OverviewPage.vue';
+import NotFoundPage from './404.vue';
 
 export const routes: RouteRecordRaw[] = [
     {
@@ -12,6 +13,10 @@ export const routes: RouteRecordRaw[] = [
             {
                 path: '/',
                 component: OverviewPage
+            },
+            {
+                path: '/:pathMatch(.*)*',
+                component: NotFoundPage
             }
         ]
     }

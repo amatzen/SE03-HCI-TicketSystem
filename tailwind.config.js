412de0f24035bb6f2a68e779050853a28267c682
Alexander Matzen <alexander@amatzen.dk>
2021-11-30 09:53:02 +0100

    Modified tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,34 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        indigo: {
+          100: "#a8b1fb",
+          200: "#9ba6fb",
+          300: "#8f9bfa",
+          400: "#8390fa",
+          500: "#8390fa",
+          600: "#7581e1",
+          700: "#6873c8",
+          800: "#5b64af",
+          900: "#4e5696",
+        },
+        crimson: {
+          500: "#e60f4f",
+        },
+        tealish: {
+          500: "#24d2b3",
+        },
+        yellowish: {
+          500: "#fac748",
+        }
+      }
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/aspect-ratio'),
   ],
 }

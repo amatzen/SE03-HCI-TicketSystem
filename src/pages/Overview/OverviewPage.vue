7e9b8ac8d03c54140bc2ff64e4d2341bc5081e0c
Daniel Bahrami <daniel@bahrami.dk>
2021-11-30 10:42:00 +0100

    Overview setup

--- src/pages/Overview/OverviewPage.vue
+++ src/pages/Overview/OverviewPage.vue
@@ -1,13 +1,49 @@
 <template>
-  <h1>test</h1>
+  <div class="content">
+    <div class="group">
+      <h3>{{ data }}</h3>
+      <li v-for="cases in data" :key="data.title">
+        {{ data.title }}
+      </li>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "OverviewPage"
-}
+  name: "OverviewPage",
+
+  data() {
+    return {
+      data,
+    };
+  },
+};
+
+const data = {
+  data: [
+    {
+      title: "Nye sager",
+      tickets: [
+        {
+          id: 524,
+          title: "Lobortis",
+          name: "Kristian Bech",
+        },
+        {
+          id: 552,
+          title: "Integer",
+          name: "Frederikke Hjort",
+        },
+      ],
+    },
+    {
+      title: "Aktive sager",
+      color: "red",
+      tickets: [],
+    },
+  ],
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>

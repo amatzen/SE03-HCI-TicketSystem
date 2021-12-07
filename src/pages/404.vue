c6ceefc4d230f8fc289e312e455c25ec741e78db
Daniel Bahrami <daniel@bahrami.dk>
2021-12-07 11:00:32 +0100

    Update h1 color

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center">
-    <h1 class="text-6xl font-bold text-tealish-800 mb-2">404</h1>
+    <h1 class="text-6xl font-bold text-tealish-500 mb-2">404</h1>
     <p class="text-2xl font-semibold text-gray-800">Vi kunne ikke finde den efterspurgte side</p>
     
   </div>

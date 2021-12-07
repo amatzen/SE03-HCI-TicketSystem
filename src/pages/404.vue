940d6929b575129faa172a6cfedb70d66385194f
Daniel Bahrami <daniel@bahrami.dk>
2021-12-07 16:19:09 +0100

    Update 404.vue

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="text-center">
-    <h1 class="text-6xl font-bold text-crimson-500 mb-2">404</h1>
+    <h1 class="text-6xl font-bold mb-2">404</h1>
     <p class="text-2xl font-semibold text-gray-800 mb-5">
       Vi kunne ikke finde den efterspurgte side
     </p>
-
-    <Button variant="crimson" @click="goBack()">Gå tilbage</Button>
+    <Button variant="tealish" @click="goBack()">Gå tilbage</Button>
   </div>
 </template>
 

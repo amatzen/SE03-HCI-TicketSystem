e7999437fcecc0b0abbe2cd142dd2c0e635feae4
Alexander Matzen <alexander@amatzen.dk>
2021-12-07 10:44:26 +0100

    Temp 404 page

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,10 +1,8 @@
 <template>
-  <div>
-    <h1 class="h1">404</h1>
-    <p class="p">Vi kunne ikke finde den efterspurgte side</p>
-    <div class="a">
-      <a href="/">Gå til forsiden</a>
-    </div>
+  <div class="text-center">
+    <h1 class="text-6xl font-bold text-tealish-800 mb-2">404</h1>
+    <p class="text-2xl font-semibold text-gray-800">Vi kunne ikke finde den efterspurgte side</p>
+    
   </div>
 </template>
 
@@ -13,28 +11,3 @@ export default {
   name: "NotFoundPage",
 };
 </script>
-
-<style scoped>
-.h1 {
-  text-align: center;
-  font-size: 10em;
-  font-family: Arial, Helvetica, sans-serif;
-  color: #24d2b3;
-}
-
-.p {
-  text-align: center;
-  font-size: 3em;
-  font-family: Arial, Helvetica, sans-serif;
-  margin-top: 2em;
-}
-
-.a {
-  text-align: center;
-  font-size: 3em;
-  font-family: Arial, Helvetica, sans-serif;
-  margin-top: 1em;
-  color: #24d2b3;
-  text-decoration: underline;
-}
-</style>
